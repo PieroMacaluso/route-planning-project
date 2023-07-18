f068ec846bef7ab3da5e065f4e75d51d6431cfbd
Piero Macaluso <piero.macaluso@stellantis.com>
2023-07-18 21:42:38 +0200

    `NextNode` implementation

--- src/route_planner.cpp
+++ src/route_planner.cpp
@@ -43,7 +43,7 @@ void RoutePlanner::AddNeighbors(RouteModel::Node *current_node) {
 }
 
 
-// TODO 5: Complete the NextNode method to sort the open list and return the next node.
+// Complete the NextNode method to sort the open list and return the next node.
 // Tips:
 // - Sort the open_list according to the sum of the h value and g value.
 // - Create a pointer to the node in the list with the lowest sum.
@@ -51,7 +51,13 @@ void RoutePlanner::AddNeighbors(RouteModel::Node *current_node) {
 // - Return the pointer.
 
 RouteModel::Node *RoutePlanner::NextNode() {
-
+    std::sort(open_list.begin(), open_list.end(),
+            [](RouteModel::Node* a, RouteModel::Node* b) {
+                return a->h_value + a->g_value < b->h_value + b->g_value;
+            });
+    RouteModel::Node * pointer = open_list.front();
+    open_list.erase(open_list.begin());
+    return pointer;
 }
 
 
